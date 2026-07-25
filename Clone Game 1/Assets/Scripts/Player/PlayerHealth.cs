31bc3ae72612bd69661a40c3e31c36b827dfb542
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2026-07-25 21:18:46 +0200

    Health UI
    Added Health text and counter
    imported TMPro asset

--- Clone Game 1/Assets/Scripts/Player/PlayerHealth.cs
+++ Clone Game 1/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,19 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 10;
     public int health;
+    public TextMeshProUGUI healthText;
 
 
     void Start()
     {
         health = maxHealth;
+        healthText.text = "HP: " + health;
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        healthText.text = "HP: " + health;
+
         if (health <= 0)
         {
             Destroy(gameObject);
