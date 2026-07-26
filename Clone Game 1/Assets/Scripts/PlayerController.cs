47954a066071410d031dec9ec3d7f6e959f471b5
SkullManea <venturousskull@gmail.com>
2026-07-26 13:20:10 +0200

    Merge attack into playerscript

--- Clone Game 1/Assets/Scripts/PlayerController.cs
+++ Clone Game 1/Assets/Scripts/PlayerController.cs
@@ -26,6 +26,13 @@ public class PlayerController : MonoBehaviour
 
     [Header ("Attacking")]
     public float attackCost;
+    public Transform attackOrigin;
+    public float attackRadius = 1f;
+    public LayerMask enemyMask;
+    public int attackDamage;
+    public float cooldownTime = .5f;
+    public float cooldownTimer = 0f;
+
 
     private PlayerControls playerControls;
     private Vector2 movement;
@@ -38,6 +45,7 @@ public class PlayerController : MonoBehaviour
         playerControls = new PlayerControls();
         rigidBody = GetComponent<Rigidbody2D>();
         playerControls.Movement.Jump.performed += OnJump;
+        
     }
 
     private void OnEnable()
@@ -95,16 +103,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // private void OnAttack(InputAction.CallbackContext context)
-    // {
-    //     if (context.performed)
-    //     {
+    private void Attack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack");
+         if (cooldownTimer <= 0)
+        {
+            {
+                Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius, enemyMask);
+                foreach (var enemy in enemiesInRange)
+                {
+                    //enemy.GetComponent<HealthManager>().TakeDamage(attackDamage);
+                }
+
+                cooldownTimer = cooldownTime; //resets timer
+            }  
+        }
+        else
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
         
-    //     currentStamina -= attackCost;
-    //     Staminacharge();  
-    //     }
-        
-    // }
+    }
 
     void Staminacharge()
     {
@@ -130,6 +149,11 @@ public class PlayerController : MonoBehaviour
            staminaBar.fillAmount = currentStamina / maxStamina;
            yield return new WaitForSeconds(.1f);
         }
+    }
+
+    private void OnDrawGizmos() //for attacking
+    {
+        Gizmos.DrawWireSphere(attackOrigin.position, attackRadius);       
     }
 
 
