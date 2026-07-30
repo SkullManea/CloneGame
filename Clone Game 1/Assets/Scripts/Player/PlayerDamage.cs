e5bcf76c1e5f237569571fa479c57f2ff9467a74
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2026-07-30 20:47:08 +0200

    --- Enemy Health and Player Damage ---
    Moved the EnemyHeath script to the empty parent enemy object

--- Clone Game 1/Assets/Scripts/Player/PlayerDamage.cs
+++ Clone Game 1/Assets/Scripts/Player/PlayerDamage.cs
@@ -6,12 +6,20 @@ public class PlayerDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collieded with: " + collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
             EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            Debug.Log(enemyHealth);
 
             if (enemyHealth != null)
+            {
+                Debug.Log("Found!");
                 enemyHealth.TakeDamage(damage);
+            }
+            else
+                Debug.Log("Not Found");
         }
     }
 }
